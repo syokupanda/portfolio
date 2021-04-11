bcaf9c198a779ab0895828c7af6babb4766a9107
syokupanda <borun.books@gmail.com>
2021-04-11 05:34:28 +0000

    [add]model追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_053713) do
+ActiveRecord::Schema.define(version: 2021_04_11_045915) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,6 +41,21 @@ ActiveRecord::Schema.define(version: 2021_04_10_053713) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_farmers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_farmers_on_reset_password_token", unique: true
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "image_id"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "image_id"
+    t.string "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taggings", force: :cascade do |t|
