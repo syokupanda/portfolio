fa4a55b2e9201f07a1d6c391f0e07a09ca4e3dd9
syokupanda <borun.books@gmail.com>
2021-04-12 01:50:01 +0000

    [add]recipe機能（編集・削除）追加

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,12 +18,19 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe.id)
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
   end
 
 
