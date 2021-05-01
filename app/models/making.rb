18d71acc2ad1423ddab870e199c7c841658d303a
syokupanda <borun.books@gmail.com>
2021-05-01 13:39:27 +0000

    [add]validation

--- app/models/making.rb
+++ app/models/making.rb
@@ -1,4 +1,8 @@
 class Making < ApplicationRecord
   attachment :image
   belongs_to :recipe
+  
+  #--------------------------------------------
+  validates :process, presence: true
+  #--------------------------------------------
 end
