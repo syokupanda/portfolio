bc37ab7d965d270b8f689b65737a0cfd54adf44f
syokupanda <borun.books@gmail.com>
2021-05-24 01:45:55 +0000

    [save]admins contact

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -13,7 +13,19 @@ class Admins::ContactsController < ApplicationController
 
   def update
     @contact = Contact.find(params[:id])
-    @contact.update
+    @contact.update(contact_params)
+    if @contact.is_permit == true
+      @contact.update(status: 1)
+    else
+      @contact.update(status: 2)
+    end
     redirect_to admins_contact_path(@contact.id)
+  end
+
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:user_id, :is_permit, :category, :status, :return_text)
   end
 end
